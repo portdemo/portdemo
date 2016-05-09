3929ab0c1e6c9938c701bb73a756f4f9ac3bc914
Sudharsan <g.k.sudharsan@accenture.com>
2016-05-09 19:17:41 +0530

    Scaffolded. Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portalApp
+ * @description
+ * # portalApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portalApp', [
+    'ngAnimate',
+    'ngMessages',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'ui.bootstrap'
+  ]);
