d5e365451646c1ab565b1c7653f0a3c83e157ccb
Krishnan <gokula.n.krishnan@accenture.com>
2016-05-10 14:50:24 +0530

    added ui route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,10 +10,30 @@
  */
 angular
   .module('portalApp', [
-    'ngAnimate',
-    'ngMessages',
-    'ngSanitize',
-    'ngTouch',
     'ui.router',
     'ui.bootstrap'
-  ]);
+  ]).config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'views/login.html',
+        //controller: 'MainCtrl'
+      })
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/dashboard.html',
+        //controller: 'dashboardCtrl'
+      })
+      .state('claims',{
+        url: '/claims',
+        templateUrl: 'views/claims.html',
+        //controller: 'claimsCtrl'
+      })
+      .state('register',{
+        url: '/register',
+        templateUrl: 'views/register.html',
+        //controller: 'registerCtrl'
+      });   
+
+    $urlRouterProvider.otherwise('/login');
+  });
