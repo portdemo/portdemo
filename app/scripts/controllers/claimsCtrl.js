e21318335830abc56ea9500403384015d8cd4940
Harish <popuru.harish@accenture.com>
2016-05-12 15:38:31 +0530

    created seperate claimsctrlfor dashboard

--- app/scripts/controllers/claimsCtrl.js
+++ app/scripts/controllers/claimsCtrl.js
@@ -33,3 +33,43 @@ app.controller('claimsCtrl', ['$scope', '$http', function($scope, $http){
 		$scope.gridApi.expandable.collapseAllRows();
 	}*/
 }]);
+app.controller('claimsDashboardCtrl', ['$scope', '$http', function($scope, $http){
+	$scope.gridOptions = {
+	    expandableRowTemplate: '<div style="height:150px;">Sundar</div>',
+	    expandableRowHeight: 100
+	};
+//$scope.data={};
+	$scope.gridOptions.columnDefs = [
+		{ name: 'ServiceDate'},
+		{ name: 'Patient'},
+		{ name: 'Provider'},
+		{ name: 'TotalBilled'},
+		{ name: 'Status'}
+	];
+
+	$scope.gridOptions.onRegisterApi = function(gridApi){
+		$scope.gridApi = gridApi;
+	};
+
+	$http.get('/data/claimsData.json')
+    .success(function(data) {
+        //$scope.data = data;
+      //$scope.gridOptions.data =;
+        
+       for(var i=1; i<=3; i++)
+           {
+               $scope.data[i]=data[i];
+               
+           }
+   // alert(JSON.stringify($scope.data));
+       $scope.gridOptions.data =  $scope.data[i];
+    });
+
+	/*$scope.expandAllRows = function() {
+		$scope.gridApi.expandable.expandAllRows();
+	}
+
+	$scope.collapseAllRows = function() {
+		$scope.gridApi.expandable.collapseAllRows();
+	}*/
+}]);
