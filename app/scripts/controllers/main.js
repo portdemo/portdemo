9677ee79202bb8e6468ef459cbd9436afc92d12e
Tz <nizamuddeen.tz@accenture.com>
2016-05-12 18:01:15 +0530

    added banner data through api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,9 @@
  * Controller of the portalApp
  */
 angular.module('portalApp')
+ /*
+ * Controller for authentication for login
+ */
   .controller('loginCtrl', function ($scope, $state, $localStorage, loginService) {
     /*if($localStorage.useremail){
         alert('yes stored');
@@ -38,27 +41,54 @@ angular.module('portalApp')
         //$state.go('home');
     }
   });
+
+  /*
+   * Controller for displaying banner data
+   */
+angular.module('portalApp')
+ .controller('bannerCtrl', function($scope, $state, $localStorage, loginService){
+     if(!$localStorage.username){
+         $state.go('login');
+     }
+    loginService.getUser().then(function(data){
+        $scope.banners = data;
+        var leng = $scope.banners.length;
+        for(var i=0;i<leng;i++){
+            if($localStorage.username == $scope.banners[i].userName){
+                $scope.first_name =$scope.banners[i].firstName;
+                $scope.member_id =$scope.banners[i].memberId;
+                $scope.plan_name =$scope.banners[i].planName;
+                $scope.last_login =$scope.banners[i].lastLogin;
+            }
+        }
+    })
+ });
+
+/*
+*Created a Factory for login and banner to show data
+*
+*/
 angular.module('portalApp')
     .factory('loginService', loginFactory);
 
-function loginFactory($http) {
-  this.$inject = ['$http'];
-  return {
-    getUser: getUser
-  };
+    function loginFactory($http) {
+      this.$inject = ['$http'];
+      return {
+        getUser: getUser
+      };
 
-  function getUser () {
-    return $http
-      .get('http://10.236.91.188:8080/SpringRestfulWebServicesWithJSONExample/member')
-      .then(complete)
-      .catch(failed);
-  }
+      function getUser () {
+        return $http
+          .get('http://10.236.91.188:8080/SpringRestfulWebServicesWithJSONExample/member')
+          .then(complete)
+          .catch(failed);
+      }
 
-  function complete(response) {
-    return response.data;
-  }
+      function complete(response) {
+        return response.data;
+      }
 
-  function failed(error) {
-    return error.statusText;
-  }
-}
+      function failed(error) {
+        return error.statusText;
+      }
+    }
