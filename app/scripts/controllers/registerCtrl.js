c20844fa8857b2ef85b4e6b0f2d16d7f2909b900
Sudharsan <g.k.sudharsan@accenture.com>
2016-05-13 12:46:42 +0530

    added error msg sample to registration page

--- app/scripts/controllers/registerCtrl.js
+++ app/scripts/controllers/registerCtrl.js
@@ -9,15 +9,34 @@ app.controller('registerCtrl',['$scope','$state', /*AuthService,*/ function($sco
 
 // Need to add register button action function and reset status.
 $scope.startFade=true;
-$scope.hidden=true;
+$scope.showFailure=false;
+$scope.showSuccess=false;
+$scope.showStatus=false;
+
 $scope.formReset=function(){
 	  $scope.signupForm.$setPristine();
               $scope.signupForm.$setUntouched();
 }
+
+
+
 $scope.registerUser=function(){
 	console.log($scope.newUser);
-	$scope.hidden=false; 
-	$scope.statusMsg="User has been registered successfully!";
-	$scope.regStatus=true;
+		$scope.showStatus=true;
+
+	$scope.showSuccess=true; 
+	$scope.statusMsg="Your account has been created successfully!";
+
+
+  //Just to see failure msg:
+    $scope.showStatus=true;
+    $scope.showSuccess=false;
+    $scope.showFailure=true;
+   $scope.statusMsg="Account registration has failed. Please try again!";
 }
+
+$scope.closeAlert=function(){
+	$scope.showFailure=false;
+}
+
 }]);
