3ac6e71156e3aef7beaf8559be30d95ea63bfbf8
Sudharsan <g.k.sudharsan@accenture.com>
2016-05-18 12:11:24 +0530

    added a check for already logged in user

--- app/scripts/controllers/registerCtrl.js
+++ app/scripts/controllers/registerCtrl.js
@@ -3,17 +3,20 @@
 
 'use strict';
 var app=angular.module('portalApp');
-app.controller('registerCtrl',['$scope','$state', '$http','registerService', function($scope,$state,$http, registerService){
+app.controller('registerCtrl',['$scope','$state', '$http','registerService', '$localStorage', function($scope,$state,$http, registerService, $localStorage){
 //If user is authenticated already, redirected to home page.  
 /* if(Authentication.isauthenticated()===true){
               $state.go('home');  
           }  */
 
+  if($localStorage.username){
+         $state.go('home');
+     }  
 
 // Need to add register button action function and reset status.
 $scope.startFade=true;
 $scope.showFailure=false;
-$scope.showSuccess=false;
+$scope.showSuccess=false; 
 $scope.showStatus=false;
 
 $scope.formReset=function(){
@@ -24,17 +27,10 @@ $scope.formReset=function(){
 
 
 $scope.registerUser=function(){
-//	console.log(angular.toJson($scope.newUser));
 $scope.newUser.memberId="3";
 $scope.newUser.planName="testing";
 $scope.newUser.lastLogin=new Date();
-//console.log($scope.newUser.lastLogin);
-//console.log(angular.toJson($scope.newUser));
-/*$http.post('http://10.236.91.188:8080/ClaimsPortal/requestbody',angular.toJson($scope.newUser))
-		.success(function(data, status){
-			console.log(data);
-			console.log(status);
-		}); */
+
 registerService.registerUser(angular.toJson($scope.newUser)).then(function(status){
 	$scope.showStatus=true;
 	//console.log(status);
@@ -51,24 +47,12 @@ registerService.registerUser(angular.toJson($scope.newUser)).then(function(statu
 });
 
 
-	/*
-		$scope.showStatus=true;
-
-	$scope.showSuccess=true; 
-	$scope.statusMsg="Your account has been created successfully!";
-
-
-  //Just to see failure msg:
-    $scope.showStatus=true;
-    $scope.showSuccess=false;
-    $scope.showFailure=true;
-   $scope.statusMsg="Account registration has failed. Please try again!"; */
 };
 
 $scope.closeAlert1=function(){
 	$scope.showSuccess=false;
 	$state.go('login');
-}
+};
 
 $scope.closeAlert2=function(){
 	$scope.showFailure=false;
@@ -79,29 +63,27 @@ $scope.closeAlert2=function(){
 
 
 
-
-app.factory('registerService', registerFactory);
-
     function registerFactory($http) {
-//      this.$inject = ['$http'];
-      return {
-        registerUser: registerUser
-              };
 
-      function registerUser(data) {
-      	//console.log(data);
+    	function registerUser(data) {
+      	console.log(data);
        	return $http
        	.post('http://10.236.91.188:8080/ClaimsPortal/requestbody',data)
 		.then(success)
-		.catch(error);
+		.catch(failure);
 	}
 	function success(response){
 		//console.log(status + " Success");
 		return response.data;  //return data
 		}
-	function error(error){
+	function failure(error){
 		//console.log(error.status + " Failure");
 		return error.status; 
 	}
+//      this.$inject = ['$http'];
+      return {
+        registerUser: registerUser
+              }; 
 
-}
+      }
+app.factory('registerService', registerFactory);
