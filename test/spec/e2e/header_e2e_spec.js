30d62c713e76171d5fca8670e44f59af9ce30fc5
Sridharan <kannan.a.sridharan@accenture.com>
2016-05-19 14:50:13 +0530

    header e2e test

--- test/spec/e2e/header_e2e_spec.js
+++ test/spec/e2e/header_e2e_spec.js
@@ -1,15 +1,3 @@
-/*
-Create an e2e spec that tests TODO MVC.
-
-1. Install protractor globally with `npm install -g protractor`
-2. Run `webdriver-manager update`, then `webdriver-manager start` to setup the Selenium server.
-3. Use Jasmine syntax to setup the test suite.
-4. Use the URL: http://todomvc.com/examples/angularjs/#/
-5. Use the commands: browser.get, element, by.model, by.repeater, browser.actions().sendKeys(protractor.Key.ENTER).perform();
-*/
-
-//Write your e2e test here!
-
 describe('Testing Static Page', function() {
   loginURL = 'http://localhost:9000/#/login';
   //beforeEach(browser.get('http://localhost:9000/#/home'));
