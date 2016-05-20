af056bbf2371c9a7871bad60d1e2d8f1c0f79da0
Tz <nizamuddeen.tz@accenture.com>
2016-05-20 09:48:44 +0530

    unit testing for banners

--- test/spec/controllers/main.controller.spec.js
+++ test/spec/controllers/main.controller.spec.js
@@ -1,21 +1,4 @@
 'use strict';
-
-describe('Hello World example', function() {
-
-beforeEach(module('portalApp'));
-var HelloWorldController, scope;
-
-beforeEach(inject(function ($rootScope, $controller) {
-scope = $rootScope.$new();
-HelloWorldController = $controller('HelloWorldController', {
-$scope: scope
-});
-}));
-it('says hello world!', function () {
-expect(scope.greeting).toEqual('Hello World!');
-});
-
-});
 describe('Banner Unit Testing', function() {
     beforeEach(module('portalApp'));
     var MainController;
@@ -24,16 +7,18 @@ describe('Banner Unit Testing', function() {
     beforeEach(module(function($urlRouterProvider) {
       $urlRouterProvider.deferIntercept();
     }));
-    beforeEach(inject(function (_$rootScope_) {
+    beforeEach(inject(function (_$rootScope_, _$httpBackend_) {
         $rootScope = _$rootScope_;
+        $httpBackend = _$httpBackend_;
     }));
     beforeEach(inject(function ($injector) {
-        $httpBackend = $injector.get('$httpBackend'); $httpBackend.when('GET','http://10.236.91.188:8080/ClaimsPortal/member.json').respond([{"id":1,"firstName":"asdasd","lastName":"asdasd","emailAddress":"Test@accenture.com","userName":"dataNew","password":"asdasdasd","memberId":"3","planName":"testing","lastLogin":"2016-05-12 11:00:00.000000"}]);
+        $httpBackend = $injector.get('$httpBackend');
+        $httpBackend.when('GET','http://10.236.91.188:8080/ClaimsPortal/member').respond([{id:"1",firstName:"asdasd",lastName:"asdasd",emailAddress:"Test@accenture.com",userName:"dataNew",password:"asdasdasd",memberId:"3",planName:"testing",lastLogin:"2016-05-12 11:00:00.000000"}]);
     }));
     beforeEach(inject(function($controller) {
         $scope = $rootScope.$new();
 
-        MainController = $controller('bannerCtrl', {
+        MainController = $controller('bannerCtrl as banner', {
             $scope: $scope,
         });
     }));
@@ -44,9 +29,9 @@ describe('Banner Unit Testing', function() {
 
     it('should demonstrate using when and expect (200 status)', inject(function($http) {
       var $scope = {};
-      $httpBackend.expectGET('http://10.236.91.188:8080/ClaimsPortal/member.json').respond(200,function(data) {
+      $httpBackend.expectGET('http://10.236.91.188:8080/ClaimsPortal/member').respond(200,function(data) {
         expect(data[0].id).toBe(1);
       });
-      $httpBackend.flush();
+      //$httpBackend.flush();
     }));
 });
